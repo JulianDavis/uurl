4d37ad072bda89f19dc159227d37d64a68bba7d4
Julian Davis <iiheartbacon@gmail.com>
2025-05-11 00:42:59 -0400

    fixup! Update README
    Remove commented code in integration test

--- uurl/test.c
+++ uurl/test.c
@@ -15,19 +15,10 @@ int main(void)
 
     if (!uurl_connect(uurl, HTTP_SERVER_IP, HTTP_SERVER_PORT, 3))
         return 1;
+
     uurl_get_request(uurl, HTTP_SERVER_ROOT"/hello");
-
-    //if (!uurl_connect(uurl, HTTP_SERVER_IP, HTTP_SERVER_PORT, 3))
-    //    return 1;
     uurl_get_request(uurl, HTTP_SERVER_ROOT"/headers");
-
-    //if (!uurl_connect(uurl, HTTP_SERVER_IP, HTTP_SERVER_PORT, 3))
-    //    return 1;
     uurl_get_request(uurl, HTTP_SERVER_ROOT"/not-a-file");
-
-    //const char *post_data = "Hello, World!";
-    //uurl_connect(uurl, "httpbin.org", 3);
-    //uurl_post_request(uurl, "/post", (uint8_t *)post_data, strlen(post_data));
 
     uurl_free(uurl);
 }
